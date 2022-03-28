4cfe630b353ff62fb5758364998bb3323134abcc
YuliethHuerfanoPerez <39287918+YuliethHuerfanoPerez@users.noreply.github.com>
2022-03-28 09:07:06 -0500

    Cambio en el nav de investigadores

--- views/admin-investigator.php
+++ views/admin-investigator.php
@@ -116,11 +116,11 @@
         </div>
     </div>
     <!-- =============== nav =============== -->
-    <!--<nav id="mainNav" class="navbar navbar-default navbar-fixed-top" >
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top" >
         <div class="container" >
             <div class="container-fluid" style="background-color: #1EA078;"> 
                 <!-- Brand and toggle get grouped for better mobile display -->
-                <!--div class="navbar-header">
+                <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
@@ -133,7 +133,7 @@
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
 
-                <!--<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav navbar-right">
 						<li>
                             <a class="page-scroll" href="admin-news.php">Noticias</a>
@@ -154,10 +154,10 @@
                 </div>
                 <!-- =============== navbar-collapse =============== -->
 
-          <!-- </div>
+           </div>
         </div>
         <!-- =============== container-fluid =============== -->
-    <!--</nav>-->
+    </nav>
     <BR></BR>
     <BR></BR>
     <BR></BR>
